6017d2ac7602bd8e95bd32e4e25df6e290d804c2
prajapatianil77 <anilprajapatbkbiet2022@gmail.com>
2024-07-03 11:05:55 +0530

    timmer

---
+++ Timmer/script.js
@@ -0,0 +1,29 @@
+let startbtn = document.querySelectorAll("button")[0]
+let stopbtn = document.querySelectorAll("button")[1]
+let resetbtn = document.querySelectorAll("button")[2]
+let box = document.querySelector(".box")
+
+let sec = 0;
+let min = 0;
+let interval=0;
+startbtn.addEventListener('click',function(){
+    if(!interval){
+   interval=setInterval(()=>{
+            sec++
+            if(sec==60){
+                sec=0;min++
+            }
+            box.innerHTML =  `<h1>${min} : ${sec}</h1>`
+        },1000);
+    }
+})
+stopbtn.addEventListener('click',()=>{
+    clearInterval(interval)
+    interval=0
+})
+resetbtn.addEventListener('click',()=>{
+    min=0;
+    sec=0;
+    box.innerHTML = `<h1>${min} : ${sec}</h1>`
+    
+})
